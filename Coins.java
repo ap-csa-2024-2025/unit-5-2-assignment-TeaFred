9c1e2f0ff337309ba05b2994637db463573d88fb
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-09-19 14:14:24 -0700

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -2,9 +2,25 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    coinConverter(1234);
   }
 
   // write code for coinConverter
+  public static String coinConverter(double a)
+  {
+    double money = (int)(a/(100);
+    int dollarBills = (int)(money);
+    double coinChange = money - (int)dollarBills; 
+    int toInt = (int)(coinChange * 100);
+    int quarterNum = toInt%25; 
+    int almFinal = toInt - (quarternum*25);
+    int dimeNum = almFinal%10; 
+    int semiFinal = almFinal - (dimeNum*10);
+    int nickelNum = semiFinal%5;
+    int theFinal = semiFinal - (nickelNum * 5);
+    int pennyNum = theFinal%1;
+    System.out.println(dollarBills + " " + quaterNum + " " + dimeNum + " " + nickelNum + " " + pennyNum);
+    
+    
 
 }
